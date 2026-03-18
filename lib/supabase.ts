0ca1fd198eb50da123d9fc62faea2fcce72e182b
ALEX <alex@Mac.lan>
2026-03-18 12:18:15 +0100

    fix: Vercel SSR config

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? 'https://lxlvcwwvnujfbqgcfzze.supabase.co';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? 'sb_publishable_Mtnk8ImM2KGy4XoLSHPtBg_zULZWV09';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
